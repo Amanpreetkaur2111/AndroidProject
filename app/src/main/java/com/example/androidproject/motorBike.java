c3e96a3bc2d7c3bddc6f1503906a9f4ac05de9c9
Amanpreetkaur <aman021011@gmail.com>
2019-12-16 12:17:14 -0500

    Add MotorBike class

---
+++ app/src/main/java/com/example/androidproject/motorBike.java
@@ -0,0 +1,28 @@
+package com.example.androidproject;
+
+public class motorBike extends E_Vehicle {
+
+    private boolean sideCar;
+
+    public motorBike(String make, String plate, String color,boolean sideCar) {
+        super(make, plate, color);
+        this.sideCar = sideCar;
+    }
+
+    public String isSideCar(){
+        if(sideCar){
+            return " With SideCar";
+        }
+        else {
+            return "Without sidecar";
+        }
+    }
+
+    public String data(){
+        String data = super.data() + "motorCycle \n" + "Model" + super.getMake() +"\n"
+                       + "Plate = " + super.getPlate() + "Color = " + super.getColor() + "\n";
+        data += "_" + isSideCar();
+        return data;
+    }
+
+}
